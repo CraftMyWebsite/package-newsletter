ee5ffc8ea460e249c8dc90238fcd48cb0189098a
Teyir <60007549+Teyir@users.noreply.github.com>
2023-12-10 12:16:26 +0100

    [IMPROVES] Update to new Package class configuration

---
+++ Package.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CMW\Package\Newsletter;
+
+use CMW\Manager\Package\IPackageConfig;
+use CMW\Manager\Package\PackageMenuType;
+use CMW\Manager\Package\PackageSubMenuType;
+
+class Package implements IPackageConfig
+{
+    public function name(): string
+    {
+        return "Newsletter";
+    }
+
+    public function version(): string
+    {
+        return "0.0.3";
+    }
+
+    public function authors(): array
+    {
+        return ["Zomb"];
+    }
+
+    public function isGame(): bool
+    {
+        return false;
+    }
+
+    public function isCore(): bool
+    {
+        return false;
+    }
+
+    public function menus(): ?array
+    {
+        return [
+            new PackageMenuType(
+                lang: "fr",
+                icon: "fas fa-bullhorn",
+                title: "Newsletter",
+                url: "newsletter/manage",
+                permission: "newsletter.show",
+                subMenus: []
+            ),
+            new PackageMenuType(
+                lang: "en",
+                icon: "fas fa-bullhorn",
+                title: "Newsletter",
+                url: "newsletter/manage",
+                permission: "newsletter.show",
+                subMenus: []
+            )
+        ];
+    }
+}
