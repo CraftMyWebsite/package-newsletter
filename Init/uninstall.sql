0eac9bf34104ab880396717b909edfccdf507647
Teyir <60007549+Teyir@users.noreply.github.com>
2024-06-21 11:06:02 +0200

    [IMPROVE] Add Package uninstall

---
+++ Init/uninstall.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS `cmw_newsletter_external_users`;
+DROP TABLE IF EXISTS `cmw_newsletter_users`;
+DROP TABLE IF EXISTS `cmw_newsletter_settings`;
+DROP TABLE IF EXISTS `cmw_newsletter`;
